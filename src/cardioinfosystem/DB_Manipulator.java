802bb732eb3d0a08d3d528a70299adfc1c41bfe5
Chris <Chris@Handles.wireless.rit.edu>
2015-05-05 15:43:41 -0400

    DB_Manipulator: remade as static again, created init() method Main: uses DB_Manipulator as static again
    General changes: code cleanup

--- src/cardioinfosystem/DB_Manipulator.java
+++ src/cardioinfosystem/DB_Manipulator.java
@@ -8,233 +8,203 @@ package cardioinfosystem;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Date;
 
 /**
  *
- * @author Student
+ * @author Ethan Applebee
  */
-public class DB_Manipulator 
-{
-    private  Statement stmt = null;
-    private  ResultSet rs = null;
-    private  Connection connection;
-    private  PreparedStatement prepStmt;
-    private  final SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmm"); //HL7 standard time format
+public class DB_Manipulator {
 
-    public DB_Manipulator() throws ClassNotFoundException, SQLException {
-        //Register the JDBC driver for MySQL.
-        Class.forName("com.mysql.jdbc.Driver");
-        // Use localhost or 127.0.0.1 to point to your machine
-        String url = "jdbc:mysql://127.0.0.1:3306/CIS_DB";
-        String userName = "cisman";
-        String password = "cisman";
-        connection = DriverManager.getConnection(url, userName, password);
-        stmt = connection.createStatement();
-    }            
-       
-        public boolean checkCISID(String cis_id) throws SQLException {
-            
-            boolean exists = true;
+	private static ResultSet rs = null;
+	private static Connection connection;
+	private static PreparedStatement prepStmt;
+	private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmm"); //HL7 standard time format
 
-            try {
+	private DB_Manipulator(){}
 
-                /*
-                 *  The Prepared Statement will pull out resultset of MRN
-                 */
-                prepStmt = connection.prepareStatement("SELECT cis_id FROM patient_info WHERE  = " + cis_id);
+	public static void init() throws SQLException, ClassNotFoundException {
+		//Register the JDBC driver for MySQL.
+		Class.forName("com.mysql.jdbc.Driver");
+		// Use localhost or 127.0.0.1 to point to your machine
+		String url = "jdbc:mysql://127.0.0.1:3306/CIS_DB";
+		String userName = "cisman";
+		String password = "cisman";
+		connection = DriverManager.getConnection(url, userName, password);
+	}
 
-                @SuppressWarnings("LocalVariableHidesMemberVariable")
-                ResultSet rs = prepStmt.executeQuery();
-                
-                if (!rs.next()) {
-                    exists = false;
-                }
-                
-            } catch (SQLException se) {
-                System.out.println("Error in DB_Manipulator.checkCSID: " + se);
-            } finally {
-                prepStmt.close();            
-            }
-            return exists;
-        }
-        
-        public String createFillerNumber(String cis_id, String sender_num, String order_content) throws SQLException {
-            
-            String filler_num = "";
-            
-            try {
+	public static boolean checkCISID(String cis_id) throws SQLException {
+		boolean exists = true;
 
-                /*
-                 *  The Prepared Statement will insert sender_id, order content,
-                 */
-                                
-                prepStmt = connection.prepareStatement(
-                        "INSERT INTO TABLE order (cpoe_id, order_content, cis_id) VALUES (" + sender_num + ", " + order_content + ", " + cis_id + " where cis_id = " + cis_id + ")");
+		try {
 
-                rs = prepStmt.executeQuery();
-                
-                prepStmt = connection.prepareStatement(
-                        "SELECT cis_order_id FROM order WHERE cis_id = " + cis_id); 
-                
-                rs = prepStmt.executeQuery();
-                
-                while(rs.next())
-                {
-                    filler_num = rs.getString(1);
-                }
-                
-            } catch (SQLException se) {
-                System.out.println("Error in DB_Manipulator.createFillerNumber: " + se);
-            } finally {
-                prepStmt.close();            
-            }
-            return filler_num;
-        }
-        
-        public String getDataSN(String cis_id) throws SQLException {
+			/*
+			 *  The Prepared Statement will pull out resultset of MRN
+			 */
+			prepStmt = connection.prepareStatement("SELECT cis_id FROM patient_info WHERE  = " + cis_id);
+			rs = prepStmt.executeQuery();
+			if (!rs.next()) {
+				exists = false;
+			}
 
-            String filler = createFillerNumber(cis_id, "", ""); // generates filler number
-            
-            String mrn = "";
-            String name = "";
-            String dob = "";
-            String sex = "";
-            String address ="";
-            String homeph = "";
-            String workph = "";
-            String patient_acc = "";
-            String orc_filler = filler;
-            String orc_dttrans = assignDateTime();
-            String obr_filler = filler;
-            String obr_uid = "Echo";
-            String obr_obsdt = "";
-            String provider_id = "";
+		} catch (SQLException se) {
+			System.out.println("Error in DB_Manipulator.checkCSID: " + se);
+		} finally {
+			prepStmt.close();
+		}
+		return exists;
+	}
 
+	public static String createFillerNumber(String cis_id, String sender_num, String order_content) throws SQLException {
+		String filler_num = "";
 
-            try {
+		try {
 
-                // get the patient data
-                
-                prepStmt = connection.prepareStatement("SELECT Ident_List, Pt_Name, Pt_DOB, Pt_DOB, Pt_Address, Home_Phone, Work_Phone, Pt_Acct_Num FROM patient WHERE cis_id = " + cis_id);
+			/*
+			 *  The Prepared Statement will insert sender_id, order content,
+			 */
+			prepStmt = connection.prepareStatement(
+					"INSERT INTO TABLE order (cpoe_id, order_content, cis_id) VALUES ("
+					+ sender_num + ", " + order_content + ", " + cis_id + " where cis_id = " + cis_id + ")");
+			prepStmt.execute();
 
-                @SuppressWarnings("LocalVariableHidesMemberVariable")
-                ResultSet rs = prepStmt.executeQuery();
-                
-                while(rs.next())
-                {
-                    mrn = rs.getString(1);
-                    name = rs.getString(2);
-                    dob = rs.getString(3);
-                    sex = rs.getString(4);
-                    address = rs.getString(5);
-                    homeph = rs.getString(6);
-                    workph = rs.getString(7);
-                    patient_acc = rs.getString(8);
-                }
-                
-                // get the doctor id
+			prepStmt = connection.prepareStatement(
+					"SELECT cis_order_id FROM order WHERE cis_id = " + cis_id);
+			rs = prepStmt.executeQuery();
 
-                prepStmt = connection.prepareStatement(
-                "SELECT Doctor_ID FROM visit WHERE CIS_ID = " + cis_id);
+			while (rs.next()) {
+				filler_num = rs.getString(1);
+			}
 
-                @SuppressWarnings("LocalVariableHidesMemberVariable")
-                ResultSet second_rs = prepStmt.executeQuery();
-                
-                while(second_rs.next())
-                {
-                    provider_id = rs.getString(1);
-                }
+		} catch (SQLException se) {
+			System.out.println("Error in DB_Manipulator.createFillerNumber: " + se);
+		} finally {
+			prepStmt.close();
+		}
+		return filler_num;
+	}
 
-            } catch (SQLException se) {
-                System.out.println("Error in DB_Manipulator.getDataSN: " + se);
-            } finally {
-                prepStmt.close();            
-            }
-            return mrn + "$" + name + "$" + dob + "$" + sex + "$" + address + "$" + homeph + "$" + workph + "$" 
-                    + patient_acc + "$" + orc_filler + "$" + orc_dttrans + "$" + obr_filler + "$" + obr_uid + "$" + assignDateTime() + "$" + provider_id;
-        }   
-        
-        public String getResults(String cis_id) throws SQLException {
-            
-            String mrn = "";
-            String name = "";
-            String dob = "";
-            String sex = "";
-            String address ="";
-            String homeph = "";
-            String workph = "";
-            String patient_acc = "";
-            String orc_filler = "";
-            String orc_dttrans = assignDateTime();
-            String obr_filler = "";
-            String obr_uid = "";
-            String status = "";
-            String obr_obsdt = "";
-            String provider_id = "";
-            String result_value = "";
+	public static String getDataSN(String cis_id) throws SQLException {
+		String filler = createFillerNumber(cis_id, "", ""); // generates filler number
 
+		String mrn = "";
+		String name = "";
+		String dob = "";
+		String sex = "";
+		String address = "";
+		String homeph = "";
+		String workph = "";
+		String patient_acc = "";
+		String orc_filler = filler;
+		String orc_dttrans = assignDateTime();
+		String obr_filler = filler;
+		String obr_uid = "Echo";
+//		String obr_obsdt = ""; Variable is not used, time is generated when msg is sent
+		String provider_id = "";
 
-            try {
-                
-                // getting the patient data
-                                
-                prepStmt = connection.prepareStatement(
-                        "SELECT Ident_List, Pt_Name, Pt_DOB, Pt_DOB, Pt_Address, Home_Phone, Work_Phone, Pt_Acct_Num FROM patient WHERE ident_list = " + cis_id);
+		try {
+			// get the patient data
+			prepStmt = connection.prepareStatement("SELECT Ident_List, Pt_Name, Pt_DOB, Pt_DOB, Pt_Address, Home_Phone, Work_Phone, Pt_Acct_Num FROM patient WHERE cis_id = " + cis_id);
+			rs = prepStmt.executeQuery();
 
-                @SuppressWarnings("LocalVariableHidesMemberVariable")
-                ResultSet rs = prepStmt.executeQuery();
-                
-                while(rs.next())
-                {
-                    mrn = rs.getString(1);
-                    name = rs.getString(2);
-                    dob = rs.getString(3);
-                    sex = rs.getString(4);
-                    address = rs.getString(5);
-                    homeph = rs.getString(6);
-                    workph = rs.getString(7);
-                    patient_acc = rs.getString(8);
-                }
-                
-                // getting the doctor id               
-                // getting the filler number
-                
-                prepStmt = connection.prepareStatement(
-                "SELECT cis_order_id, provider_id, test_code, result_status, result_value FROM cis_order WHERE cis_id = " + cis_id);
+			while (rs.next()) {
+				mrn = rs.getString(1);
+				name = rs.getString(2);
+				dob = rs.getString(3);
+				sex = rs.getString(4);
+				address = rs.getString(5);
+				homeph = rs.getString(6);
+				workph = rs.getString(7);
+				patient_acc = rs.getString(8);
+			}
 
-                @SuppressWarnings("LocalVariableHidesMemberVariable")
-                ResultSet second_rs = prepStmt.executeQuery();
-                
-                while(second_rs.next())
-                {
-                    orc_filler = second_rs.getString(1);
-                    obr_filler = second_rs.getString(1);
-                    provider_id = second_rs.getString(2);
-                    obr_uid = second_rs.getString(3);
-                    status = second_rs.getString(4);
-                    result_value = second_rs.getString(5);
-                }
+			// get the doctor id
+			prepStmt = connection.prepareStatement(
+					"SELECT Doctor_ID FROM visit WHERE CIS_ID = " + cis_id);
+			rs = prepStmt.executeQuery();
 
-            } catch (SQLException se) {
-                System.out.println("Error in DB_Manipulator.getDataSN: " + se.getMessage());
-            } finally {
-                prepStmt.close();            
-            }
-            return mrn + "$" + name + "$" + dob + "$" + sex + "$" + address + "$" + homeph + "$" + workph + "$" 
-                    + patient_acc + "$" + orc_filler + "$" + orc_dttrans + "$" + obr_filler + "$" + obr_uid + "$" + assignDateTime() + "$" + provider_id + "$" + status + "$" + result_value;
-        }   
-        
-	private String assignDateTime() {
+			while (rs.next()) {
+				provider_id = rs.getString(1);
+			}
+
+		} catch (SQLException se) {
+			System.out.println("Error in DB_Manipulator.getDataSN: " + se);
+		} finally {
+			prepStmt.close();
+		}
+		return mrn + "$" + name + "$" + dob + "$" + sex + "$" + address + "$" + homeph + "$" + workph + "$"
+				+ patient_acc + "$" + orc_filler + "$" + orc_dttrans + "$" + obr_filler + "$" + obr_uid + "$"
+				+ assignDateTime() + "$" + provider_id; //assignDateTime() here is OBR7_OBSDT
+	}
+
+	public static String getResults(String cis_id) throws SQLException {
+
+		String mrn = "";
+		String name = "";
+		String dob = "";
+		String sex = "";
+		String address = "";
+		String homeph = "";
+		String workph = "";
+		String patient_acc = "";
+		String orc_filler = "";
+		String orc_dttrans = assignDateTime();
+		String obr_filler = "";
+		String obr_uid = "";
+		String status = "";
+//		String obr_obsdt = ""; Variable is not used, time is generated when msg is sent
+		String provider_id = "";
+		String result_value = "";
+
+		try {
+			// getting the patient data
+			prepStmt = connection.prepareStatement(
+					"SELECT Ident_List, Pt_Name, Pt_DOB, Pt_DOB, Pt_Address, Home_Phone, Work_Phone, Pt_Acct_Num FROM patient WHERE ident_list = " + cis_id);
+
+			rs = prepStmt.executeQuery();
+
+			while (rs.next()) {
+				mrn = rs.getString(1);
+				name = rs.getString(2);
+				dob = rs.getString(3);
+				sex = rs.getString(4);
+				address = rs.getString(5);
+				homeph = rs.getString(6);
+				workph = rs.getString(7);
+				patient_acc = rs.getString(8);
+			}
+
+			// getting the doctor id               
+			// getting the filler number
+			prepStmt = connection.prepareStatement(
+					"SELECT cis_order_id, provider_id, test_code, result_status, result_value FROM cis_order WHERE cis_id = " + cis_id);
+			rs = prepStmt.executeQuery();
+
+			while (rs.next()) {
+				orc_filler = rs.getString(1);
+				obr_filler = rs.getString(1);
+				provider_id = rs.getString(2);
+				obr_uid = rs.getString(3);
+				status = rs.getString(4);
+				result_value = rs.getString(5);
+			}
+
+		} catch (SQLException se) {
+			System.out.println("Error in DB_Manipulator.getDataSN: " + se.getMessage());
+		} finally {
+			prepStmt.close();
+		}
+		return mrn + "$" + name + "$" + dob + "$" + sex + "$" + address + "$" + homeph + "$" + workph + "$"
+				+ patient_acc + "$" + orc_filler + "$" + orc_dttrans + "$" + obr_filler + "$" + obr_uid + "$"
+				+ assignDateTime() + "$" + provider_id + "$" + status + "$" + result_value;
+				//assignDateTime() here is OBR7_OBSDT
+	}
+
+	private static String assignDateTime() {
 		Date currentDateTime = new Date();
 		return formatter.format(currentDateTime);
 	}
-    
 }
--- src/cardioinfosystem/Main.java
+++ src/cardioinfosystem/Main.java
@@ -12,8 +12,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  *
@@ -29,86 +27,74 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-        String cis_id = "";
-        String input = "";
-        String type = "";
-        String status = "";
-        
-      // TODO code application logic here
-		/*ArrayList<String> params = new ArrayList<>();
-		 params.addAll(Arrays.asList("20150427","20150427","12345^Lambeer^Bill^^^^^^^^^^^^^^^^^MD",
-		 "1274499^^^^MR^Strong Memorial Hospital~800999^^^^MR^Highland Hospital",
-		 "Smith^John","19831002","M","742 Evergreen Terrace^^Springfield^XX^99999",
-		 "^^^^^585^5550199","^^^^^585^2752100","329123456","83400^101^01",
-		 "12345^Lambeer^Bill^^^^^^^^^^^^^^^^^MD","MED","20150421","","123",
-		 "20150427","123","ECHO^Echocardiogram","","12345",""));
-		 MsgBuilder.sendFillerNumber(params); */
+		String cis_id = "";
+		String input = "";
+
+		try {
+			DB_Manipulator.init();
+		} catch (ClassNotFoundException | SQLException ex) {
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+			System.exit(1);
+		}
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-                System.out.println("Welcome to the Cardiology Information System.");
-                
+		System.out.println("Welcome to the Cardiology Information System.");
+
 		do {
 			System.out.println("Please enter a patient's ID.\n");
 			try {
-                            cis_id = br.readLine();
-                        } 
-                        catch (IOException ioe) {
-                            System.out.print(ioe.getMessage());
-                        }
-                            
+				cis_id = br.readLine();
+			} catch (IOException ioe) {
+				System.out.print(ioe.getMessage());
+			}
+
 			/*
 			 Validate MRN
 			 */
-                        
-                        try {
-                            
-                            if (DB_Manipulator.checkCISID(cis_id)) {
+			try {
+				if (DB_Manipulator.checkCISID(cis_id)) {
+					System.out.println("What would you like to do?\n");
+					System.out.println("1 - Create SN Order\n"
+							+ "2 - Send Results\n"
+							+ "3 - Quit\n");
 
-                                System.out.println("What would you like to do?\n");
-                                System.out.println("1 - Create SN Order\n"
-                                                 + "2 - Send Results\n"
-                                                 + "3 - Quit\n");
+					try {
+						input = br.readLine();
+					} catch (IOException ioe) {
+						System.out.print(ioe.getMessage());
+					}
+					switch (input) {
+						case "1":
+							sendMsg(DB_Manipulator.getDataSN(cis_id));
+						case "2":
+							sendMsg(DB_Manipulator.getResults(cis_id));
+						case "3":
+							break;
+						default:
+							System.out.println("Invalid input.\n");
+							break;
+					}
+				} else {
 
-                                try {
-                                    input = br.readLine();
-                                }
-                                catch (IOException ioe) {
-                                    System.out.print(ioe.getMessage());
-                                }
-
-                                switch (input) {
-
-                                    case "1": sendMsg(DB_Manipulator.getDataSN(cis_id));
-
-                                    case "2": sendMsg(DB_Manipulator.getResults(cis_id));
-                                     
-                                    case "3": break;
-
-                                    default: System.out.println("Invalid input.\n"); break;
-                                }
-
-                            } else {
-
-                                System.out.println("That patient does not exist in the database.\n"); break;
-                            }
-                        }
-                        catch (SQLException sqle) {
-                            System.out.print(sqle.getMessage());
-                        }
+					System.out.println("That patient does not exist in the database.\n");
+					break;
+				}
+			} catch (SQLException sqle) {
+				System.out.print(sqle.getMessage());
+			}
 
 		} while (true);
 
 		try {
-                    br.close();
-                }
-                catch (IOException ioe)
-                {
-                    System.out.print(ioe.getMessage());
-                }
-                
-                System.out.println("Thank you, come again!");
-		System.exit(0);    
-        }
+			br.close();
+		} catch (IOException ioe) {
+			System.out.print(ioe.getMessage());
+		}
+
+		System.out.println("Thank you, come again!");
+		System.exit(0);
+	}
 
 	private static void sendMsg(String msg) {
 		try {
@@ -118,13 +104,6 @@ public class Main {
 		} catch (IOException ex) {
 			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
 		}
-		/**
-		 * Generate new filler number.
-		 *
-		 * @return
-		 */
-        }
-        
-
+	}
 
 }
